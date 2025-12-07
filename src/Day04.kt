01afd9dc92de6b64cab269c852c87a9004486abb
alfonsoristorato <alfonsoristorato@gmail.com>
2025-12-07 21:30:38 +0000

    day 4 part 2 slow + fix part 1

--- src/Day04.kt
+++ src/Day04.kt
@@ -14,25 +14,64 @@ fun main() {
         }
     }
     println("Part 1: ${okRolls.size}")
+    println("Part 2: ${checkUntilNoMoreRemovals(mapOfRolls, okRolls).size}")
 }
 
-private fun adjacentRolls(mapOfRolls: List<String>, currentRollX: Int, currentRollY: Int): Int {
+private fun checkUntilNoMoreRemovals(
+    mapOfRolls: List<String>,
+    removedRollsSoFar: List<Pair<Int, Int>>
+): List<Pair<Int, Int>> {
+    val removedRolls = removedRollsSoFar.toMutableList()
+    val removedRollsBefore = removedRolls.size
+    mapOfRolls.forEachIndexed { lineIndex, toiletRollLine ->
+        toiletRollLine.forEachIndexed { rowIndex, row ->
+            if (row == '@') {
+                val adjacentCount = adjacentRolls(mapOfRolls, rowIndex, lineIndex, removedRollsSoFar)
+                if (adjacentCount < 4) {
+                    removedRolls.add(Pair(rowIndex, lineIndex))
+                }
+            }
+        }
+    }
+    if (removedRolls.size > removedRollsBefore) {
+        checkUntilNoMoreRemovals(mapOfRolls, removedRolls)
+    }
+    return removedRolls
+}
+
+private fun adjacentRolls(
+    mapOfRolls: List<String>,
+    currentRollX: Int,
+    currentRollY: Int,
+    removedRolls: List<Pair<Int, Int>>? = null
+): Int {
     var adjacentCount = 0
 
-    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY - 1)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY + 1)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX, currentRollY - 1)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY - 1)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX, currentRollY + 1)) adjacentCount++
-    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY + 1)) adjacentCount++
+    // LEFT
+    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY, removedRolls)) adjacentCount++
+    // TOP-LEFT
+    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY - 1, removedRolls)) adjacentCount++
+    // TOP
+    if (checkRollInPosition(mapOfRolls, currentRollX, currentRollY - 1, removedRolls)) adjacentCount++
+    // TOP-RIGHT
+    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY - 1, removedRolls)) adjacentCount++
+    // RIGHT
+    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY, removedRolls)) adjacentCount++
+    // BOTTOM-RIGHT
+    if (checkRollInPosition(mapOfRolls, currentRollX + 1, currentRollY + 1, removedRolls)) adjacentCount++
+    // BOTTOM
+    if (checkRollInPosition(mapOfRolls, currentRollX, currentRollY + 1, removedRolls)) adjacentCount++
+    // BOTTOM-LEFT
+    if (checkRollInPosition(mapOfRolls, currentRollX - 1, currentRollY + 1, removedRolls)) adjacentCount++
 
     return adjacentCount
 }
 
-fun checkRollInPosition(mapOfRolls: List<String>, x: Int, y: Int): Boolean {
+fun checkRollInPosition(mapOfRolls: List<String>, x: Int, y: Int, removedRolls: List<Pair<Int, Int>>?): Boolean {
     if (y in mapOfRolls.indices && x in mapOfRolls[y].indices) {
+        if (removedRolls != null) {
+            return mapOfRolls[y][x] == '@' && !removedRolls.contains(Pair(x, y))
+        }
         return mapOfRolls[y][x] == '@'
     }
     return false
